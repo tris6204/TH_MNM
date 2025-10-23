dab0ec5c55e95278f4aaa84e9006a02116809344
Tris <77889867+tris6204@users.noreply.github.com>
2025-10-23 16:01:18 +0700

    bai8.c

---
+++ bai8.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void bai8() {
+    int n;
+    printf("--- Bai 8: Ve hinh tam giac so ---\n");
+    printf("Moi nhap chieu cao n (vd: 5): ");
+    scanf("%d", &n);
+
+
+    for (int i = 1; i <= n; i++) { 
+        
+        for (int j = 1; j <= i; j++) { 
+            printf("%d", j);
+        }
+        
+        printf("\n");
+    }
+}
+
+int main() {
+    bai8();
+    return 0;
+}
