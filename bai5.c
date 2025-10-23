387542a10c36df20864f8a3b06adb1b379e11d84
Tris <77889867+tris6204@users.noreply.github.com>
2025-10-23 16:27:26 +0700

    bai5

--- bai5.c
+++ bai5.c
@@ -2,36 +2,50 @@
 
 void bai5_TinhTienDien() {
     int soDien;
-    long long tienDien = 0;
+    int tienDien = 0; 
+    
     printf("--- Bai 5: Tinh tien dien (Theo gia dinh luy tien) ---\n");
     printf("Moi nhap so kWh tieu thu: ");
     scanf("%d", &soDien);
 
     int soDienConLai = soDien;
-
     if (soDienConLai > 0) {
-        int dienTieuThu = (soDienConLai > 100) ? 100 : soDienConLai;
-        tienDien += (long long)dienTieuThu * 500;
+        int dienTieuThu;
+        if (soDienConLai > 100) {
+            dienTieuThu = 100;
+        } else {
+            dienTieuThu = soDienConLai;
+        }
+        
+        tienDien += dienTieuThu * 500; 
         soDienConLai -= dienTieuThu;
     }
-
     if (soDienConLai > 0) {
-        int dienTieuThu = (soDienConLai > 50) ? 50 : soDienConLai;
-        tienDien += (long long)dienTieuThu * 550;
+        int dienTieuThu;
+        if (soDienConLai > 50) {
+            dienTieuThu = 50;
+        } else {
+            dienTieuThu = soDienConLai;
+        }
+        
+        tienDien += dienTieuThu * 550;
         soDienConLai -= dienTieuThu;
     }
-
     if (soDienConLai > 0) {
-        int dienTieuThu = (soDienConLai > 200) ? 200 : soDienConLai;
-        tienDien += (long long)dienTieuThu * 650;
+        int dienTieuThu;
+        if (soDienConLai > 200) {
+            dienTieuThu = 200;
+        } else {
+            dienTieuThu = soDienConLai;
+        }
+
+        tienDien += dienTieuThu * 650;
         soDienConLai -= dienTieuThu;
     }
-
     if (soDienConLai > 0) {
-        tienDien += (long long)soDienConLai * 700;
+        tienDien += soDienConLai * 700; 
     }
-
-    printf("Tong so tien dien phai tra cho %d kWh la: %lld VND\n", soDien, tienDien);
+    printf("Tong so tien dien phai tra cho %d kWh la: %d VND\n", soDien, tienDien);
 }
 
 int main() {
